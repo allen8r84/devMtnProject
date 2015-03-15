443095482e8cb2062807571e9fb5b9a5989e9017
Jon <jon@allen8r.com>
2015-03-15 00:15:27 -0600

    finish login and register linking to firebase - still needs cleaning up, but works - on to setting up routing based on login status

--- directives/login/loginConroller.js
+++ directives/login/loginConroller.js
@@ -1,17 +1,59 @@
 var app = angular.module('estateLMS');
 
-app.controller('loginConroller', function ($scope, $location, envService) {
-    $scope.env = envService.getEnv();
+app.controller('loginConroller', function ($scope, $location, envService, $window, $firebaseArray, $firebaseObject, $firebaseAuth, $timeout) {
+    var firebaseUrl = envService.getEnv().firebase;
+    var authObject = $firebaseAuth(new Firebase(firebaseUrl));
+    var moment = $window.moment;
+    
+    $scope.logIn = function(email, password) {
+		authObject.$authWithPassword({
+			email: email,
+			password: password
+		}).then(function(authData) {
+			console.log('authData', authData);
+		}, function(error) {
+			console.log('error', error);
+		});
+	};
+	
+	$scope.register = function(email, password) {
+		authObject.$createUser({
+			email: email,
+			password: password
+		}).then(function(authData) {
+			return authObject.$authWithPassword({
+				email: email,
+				password: password
+			});
+		}).then(function(authData) {
+			var userEmail = email,
+				uid = authData.uid,
+				user = $firebaseObject(new Firebase(firebaseUrl + "/users/" + uid));
+			
+			console.log('authData', authData);
 
-    $scope.status = 'Register';
+			$timeout(function() {
+				user.email = userEmail;
+				user.uid = uid;
+				user.created = moment().format();
+				user.fName = $scope.fName;
+				user.lName = $scope.lName;
+				user.lastLogon = moment().format();
+				user.$save().then(function(success) {
+					console.log('success', success);
+				}, function(error) {
+					console.log('error', error);
+				});		
+			});
+
+		}, function(error) {
+			console.log('error', error);
+		});	
+	};
+    
+    
     $scope.showReg = function(){
-     if($scope.status === 'Register'){
-         $scope.status = 'Login';
-     } else {
-        $scope.status = 'Register';
-     }
-     
-    $scope.reg = !$scope.reg;
-  };
+        $scope.reg = !$scope.reg;
+    };
 
 });

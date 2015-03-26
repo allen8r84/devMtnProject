78c2c381b7c9a2d7a3320a7b1f0a2645dbd654ec
Jon <jon@allen8r.com>
2015-03-25 20:48:30 -0600

    finish quiz integration - next is populating quiz with firebase stuff

--- courses/coursesController.js
+++ courses/coursesController.js
@@ -1,13 +1,14 @@
 var app = angular.module('estateLMS');
 
-app.controller('coursesController', function($scope, user, courses, $timeout, $location, aCourse, envService, $firebaseObject, $rootScope, $log){
+app.controller('coursesController', function($scope, user, courses, $timeout, $location, aCourse, envService, $firebaseObject, $rootScope, $log, $modal){
 //pre-defined and pre-loaded variables    
     var firebaseUrl = envService.getEnv().firebase; 
-    var time = 5; //set slide timer for questions
+    var time = 2; //set slide timer for questions
+    var quizFreq = 2; //set after how many slides a quiz should be fired
     
     $scope.i = 0;
     $scope.pb = $scope.i;
-    
+    $scope.courseTitle = $location.path().split('/').slice(3).join('');
     var user = user;
     user.$bindTo($scope, 'user')
     
@@ -28,10 +29,9 @@ app.controller('coursesController', function($scope, user, courses, $timeout, $l
         $scope.currentCourse = $scope.courseArray;
         $scope.max = aCourse.length; //max value for progress bar
         $scope.percent = 100 / $scope.max; //setting progress bar slide value
-        $scope.mytimeout = $timeout($scope.onTimeout,1000);//start slide timer at content load
+        $scope.mytimeout = $timeout($rootScope.onTimeout,1000);//start slide timer at content load
 
     });
-
 
 //end of pre-defined and pre-loaded variables    
     
@@ -43,33 +43,25 @@ app.controller('coursesController', function($scope, user, courses, $timeout, $l
     $scope.setNext();
     
     $scope.nextSlide = function(){
-        if($scope.i < ($scope.numSlides - 2)){
+        
+        if($scope.i < ($scope.numSlides - 1)){
             //pb = 2 & i = 0
             //if statement to check if next click goes to an uncompleted slide
             if($scope.pb === ($scope.i + 1)){
-                $scope.mytimeout = $timeout($scope.onTimeout,1000); //restart timer if slide hasn't been completed yet
-                $scope.counter = time;
+                $scope.mytimeout = $timeout($rootScope.onTimeout,1000); //restart timer if slide hasn't been completed yet
+                $rootScope.counter = time;
                 $scope.setNext();
                 $scope.i++;
+                
             }else if($scope.pb > ($scope.i + 1)) {
+                if($scope.pb === $scope.max && $scope.i === ($scope.pb - 2)){
+                    $rootScope.counter = "Finished!"
+                }
+                
                 $scope.i++;
+                
             }
-            
-            
-            /*if($scope.pb <= $scope.i){
-                $scope.pb++
-            }
-            $scope.i++;
-            $scope.counter = null;
-        }else{
-           if($scope.pb <= $scope.i){
-                $scope.pb++
-            }
-           $scope.i++;
-           $scope.counter = "Finished!"
-            
-        }*/
-    }
+        }
     }
     
 //end of Next button functionality    
@@ -79,35 +71,43 @@ app.controller('coursesController', function($scope, user, courses, $timeout, $l
 //Be available to click & go back to previous slide
     $scope.backSlide = function(){
             $scope.i--;
-            /*$scope.counter = time;*/
+            /*$rootScope.counter = time;*/
             $scope.next = true;
             $timeout.cancel($scope.mytimeout);
-            $scope.counter = null;    
+            $rootScope.counter = null;    
     };
 //End of Previous Button
 
 
 //timer for next button - has a countdown displayed until next slide.    
-    $scope.counter = time;
-    $scope.onTimeout = function(){
-        $scope.counter--;
-        if ($scope.counter > 0) {
-            $scope.mytimeout = $timeout($scope.onTimeout,1000);
+    $rootScope.counter = time;
+    $scope.ngifQuiz = true;
+    $rootScope.onTimeout = function(){
+
+        $rootScope.counter--;
+        if ($rootScope.counter > 0) {
+            $scope.mytimeout = $timeout($rootScope.onTimeout,1000);
         }
         else if($scope.i < ($scope.numSlides - 1)) {
             if($scope.pb <= $scope.i){
                 $scope.pb++
             }
             $scope.i++;
-            $scope.counter = time + 1;
-            $scope.onTimeout();
+            $rootScope.counter = time + 1;
+            $rootScope.onTimeout();
+            //add in quiz modal
+            if($scope.i % quizFreq === 0) {
+                $scope.open();
+            }
         }
         else {
             if($scope.pb <= $scope.i){
                 $scope.pb++
             }
-            $scope.counter = "Finished!"
+            $rootScope.counter = "Finished!"
             $scope.next = true;
+            $scope.open();
+            
         }
     }
 //end of timer for next button
@@ -118,4 +118,30 @@ app.controller('coursesController', function($scope, user, courses, $timeout, $l
         alert("Congratulations!\nYou finished this course.\nPress OK to go back to your dashboard and view otheavailable courses.")
         var path = $location.path('/dashboard/' + $scope.uid);
     };
+    
+    
+ //modal controller
+   $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+    $timeout.cancel($scope.mytimeout);
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      backdrop: 'static',
+      keyboard: false,
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
 });
--- courses/modalController.js
+++ courses/modalController.js
@@ -1,43 +1,22 @@
 var app = angular.module('estateLMS');
 
-app.controller('modalController', function ($scope, $modal, $log, $timeout) {
-
-  $scope.items = ['item1', 'item2', 'item3'];
-
-    $scope.open = function (size) {
-    $timeout.cancel($scope.mytimeout);
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      backdrop: 'static',
-      keyboard: false,
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-});
-
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
-app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items, $timeout) {
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items, $timeout, $rootScope) {
 
   $scope.items = items;
   $scope.selected = {
     item: $scope.items[0]
   };
 
-  $scope.ok = function () {
+  $rootScope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+    if($rootScope.counter !== 'Finished!'){
+        $rootScope.onTimeout();
+    }
+  };
+  $rootScope.okFinal = function () {
     $modalInstance.close($scope.selected.item);
   };
 
